6bcebb943fa7116c68bbbf35ca8b2a006e183837
sridhar762002 <121856122+sridhar762002@users.noreply.github.com>
2023-01-03 08:51:27 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,7 @@ eimport streamlit
 
 streamlit.title('My perminent Health Dineer')
 
-streamlit.header('Breakfast Menu')
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 streamlit.text('Omega 3 & Blueberry Oatmeal')
 streamlit.text('Kale, Spinach & Rocket Smoothie')
 streamlit.text('Hard-Boiled Free-Range Egg')
